a000f405f28d6d373406ca85f2744e72e2e2a3fb
victory-a <victoryasokomeh@gmail.com>
2023-12-29 01:16:09 +0100

    progress

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<!-- options api -->
+<script setup>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+</script>
+
+<!-- options api -->
+<!-- <script>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
+  }
+}
+</script> -->
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,55 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<!-- composition api v3 -->
+<script setup>
+const transactions = [
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+]
+</script>
+
+<!-- composition api -->
+<!-- <script>
+export default {
+  setup() {
+    const transactions = [
+      { id: 1, text: 'Flower', amount: -19.99 },
+      { id: 2, text: 'Salary', amount: 299.97 },
+      { id: 3, text: 'Book', amount: -10 },
+      { id: 4, text: 'Camera', amount: 150 }
+    ]
+
+    return { transactions }
+  }
+}
+</script> -->
+
+<!-- options api
+<script>
+// export default {
+//   data() {
+//     return {
+//       transactions: [
+//         { id: 1, text: 'Flower', amount: -19.99 },
+//         { id: 2, text: 'Salary', amount: 299.97 },
+//         { id: 3, text: 'Book', amount: -10 },
+//         { id: 4, text: 'Camera', amount: 150 }
+//       ]
+//     }
+//   }
+// }
+</script> -->
